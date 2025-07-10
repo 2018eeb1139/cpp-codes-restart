7fc4cdc12055c10ea597b59cb629a1613b1791e4
2018eeb1139 <amanpratapchandra1999@gmail.com>
2025-07-10 17:15:57 +0530

    Fix indentation and formatting in maxDepth and minDepth functions

--- trees/depthBinaryTree.cpp
+++ trees/depthBinaryTree.cpp
@@ -4,9 +4,26 @@ int maxDepth(TreeNode *root)
     {
         return 0;
     }
+    1 2 3
 
-    int l = maxDepth(root->left);
+        int l = maxDepth(root->left);
     int r = maxDepth(root->right);
 
     return 1 + max(l, r);
+}
+
+// min Depth little tricky
+int Solution::minDepth(TreeNode *root)
+{
+    if (root == NULL)
+    {
+        return 0;
+    }
+    int l = minDepth(root->left);
+    int r = minDepth(root->right);
+    if (root->left == NULL || root->right == NULL)
+    {
+        return 1 + max(l, r);
+    }
+    return 1 + min(l, r);
 }
