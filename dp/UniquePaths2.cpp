a88b3526a27920e9d4a28a95df6656318802d72d
2018eeb1139 <amanpratapchandra1999@gmail.com>
2025-08-07 23:15:39 +0530

    Add bottom-up dynamic programming implementation for unique paths with obstacles

--- dp/UniquePaths2.cpp
+++ dp/UniquePaths2.cpp
@@ -31,3 +31,52 @@ int uniquePathsWithObstacles(vector<vector<int>> &grid)
     memset(dp, -1, sizeof(dp)); // Corrected size of the dp array
     return solve(0, 0, grid);
 }
+
+// bottom up
+int t[101][101];
+
+int Solution::uniquePathsWithObstacles(vector<vector<int>> &A)
+{
+    int m = A.size();
+    int n = A[0].size();
+    if (A[0][0] == 1 || A[m - 1][n - 1] == 1)
+        return 0;
+    memset(t, 0, sizeof(t));
+    for (int j = 0; j < n; j++)
+    {
+        if (A[0][j] != 1)
+        {
+            t[0][j] = 1;
+        }
+        else
+        {
+            break;
+        }
+    }
+    for (int i = 0; i < m; i++)
+    {
+        if (A[i][0] != 1)
+        {
+            t[i][0] = 1;
+        }
+        else
+        {
+            break;
+        }
+    }
+    for (int i = 1; i < m; i++)
+    {
+        for (int j = 1; j < n; j++)
+        {
+            if (A[i][j] == 1)
+            {
+                t[i][j] = 0;
+            }
+            else
+            {
+                t[i][j] = t[i - 1][j] + t[i][j - 1];
+            }
+        }
+    }
+    return t[m - 1][n - 1];
+}
