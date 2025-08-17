e9f8628dc943f8244fd269155bc1d8f453f8e50f
2018eeb1139 <amanpratapchandra1999@gmail.com>
2025-08-17 10:52:16 +0530

    Add implementation of Reverse Polish Notation evaluation in Solution class

--- stack/EvalReversePolishNotation.cpp
+++ stack/EvalReversePolishNotation.cpp
@@ -26,3 +26,41 @@ int evalRPN(vector<string> &tokens)
     }
     return st.top();
 }
+
+int Solution::evalRPN(vector<string> &A)
+{
+    int n = A.size();
+    stack<int> st;
+    for (int i = 0; i < n; i++)
+    {
+        if (A[i] == "+" || A[i] == "-" || A[i] == "*" || A[i] == "/")
+        {
+            int val1 = st.top();
+            st.pop();
+            int val2 = st.top();
+            st.pop();
+            if (A[i] == "+")
+            {
+                st.push(val1 + val2);
+            }
+            if (A[i] == "-")
+            {
+                st.push(val2 - val1);
+            }
+            if (A[i] == "*")
+            {
+                st.push(val1 * val2);
+            }
+            if (A[i] == "/")
+            {
+                st.push(val2 / val1);
+            }
+        }
+        else
+        {
+            int x = stoi(A[i]);
+            st.push(x);
+        }
+    }
+    return st.top();
+}
