d699ccd3fdc702f85a6ad89200af317952529817
2018eeb1139 <amanpratapchandra1999@gmail.com>
2025-12-29 22:51:28 +0530

    add: implement findOrder function for alien dictionary problem with adjacency list and indegree tracking

--- graphs/alienDictionary.cpp
+++ graphs/alienDictionary.cpp
@@ -1,3 +1,69 @@
+
+string findOrder(vector<string> &words)
+{
+    // code here
+    string ans = "";
+    vector<int> adj[26];
+    vector<int> indegree(26, 0);
+    vector<bool> present(26, 0);
+
+    for (auto &word : words)
+    {
+        for (char c : word)
+        {
+            present[c - 'a'] = true;
+        }
+    }
+    int n = words.size();
+    for (int i = 0; i < n - 1; i++)
+    {
+        string s1 = words[i];
+        string s2 = words[i + 1];
+        int mn = min(s1.size(), s2.size());
+        bool matches = true;
+        for (int j = 0; j < mn; j++)
+        {
+            if (s1[j] != s2[j])
+            {
+                matches = false;
+                adj[s1[j] - 'a'].push_back(s2[j] - 'a');
+                indegree[s2[j] - 'a']++;
+                break;
+            }
+        }
+        if (matches && s1.size() > s2.size())
+            return "";
+    }
+    queue<int> q;
+    for (int i = 0; i < 26; i++)
+    {
+        if (present[i] && indegree[i] == 0)
+            q.push(i);
+    }
+    while (!q.empty())
+    {
+        int curr_node = q.front();
+        q.pop();
+        ans.push_back(curr_node + 'a');
+        for (int &child : adj[curr_node])
+        {
+            indegree[child]--;
+            if (indegree[child] == 0)
+            {
+                q.push(child);
+            }
+        }
+    }
+    for (int i = 0; i < 26; i++)
+    {
+        if (present[i] && indegree[i] != 0)
+        {
+            return "";
+        }
+    }
+    return ans;
+}
+
 string findOrder(vector<string> &words)
 {
     // code here
@@ -72,3 +138,5 @@ string findOrder(vector<string> &words)
         return "";
     return ans;
 }
+
+// new one
