af3f721fecaa8e5d9baeb17e3ceaadb3b6fa2244
2018eeb1139 <amanpratapchandra1999@gmail.com>
2026-01-14 16:36:30 +0530

    add: implement longestIncreasingPath function for 2D variation of LIS

--- dp/increasingPathInMatrix.cpp
+++ dp/increasingPathInMatrix.cpp
@@ -37,3 +37,44 @@ int Solution::solve(vector<vector<int>> &A)
     }
     return t[m - 1][n - 1] == 0 ? -1 : t[m - 1][n - 1];
 }
+
+int t[201][201];
+int m, n;
+bool isValid(int i, int j)
+{
+    return (i >= 0 && j >= 0 && i < m && j < n);
+}
+vector<pair<int, int>> directions = {
+    {-1, 0}, {1, 0}, {0, 1}, {0, -1}};
+int lis(int i, int j, vector<vector<int>> &a)
+{
+    if (t[i][j] != -1)
+        return t[i][j];
+    int ans = 1;
+    for (auto &dir : directions)
+    {
+        int newX = i + dir.first;
+        int newY = j + dir.second;
+        if (isValid(newX, newY) && a[i][j] < a[newX][newY])
+        {
+            ans = max(ans, 1 + lis(newX, newY, a));
+        }
+    }
+    return t[i][j] = ans;
+}
+
+int longestIncreasingPath(vector<vector<int>> &matrix)
+{
+    memset(t, -1, sizeof(t));
+    int ans = 1;
+    m = matrix.size();
+    n = matrix[0].size();
+    for (int i = m - 1; i >= 0; i--)
+    {
+        for (int j = n - 1; j >= 0; j--)
+        {
+            ans = max(ans, lis(i, j, matrix));
+        }
+    }
+    return ans;
+}
